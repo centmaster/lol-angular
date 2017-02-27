d3a8ba310c30fe3d5a0ea0edfc3b1633b80634ed
centmaster <ytchen@bupt.edu.cn>
2017-02-27 22:47:08 +0800

    nav-mid

--- js/controller/ekko.js
+++ js/controller/ekko.js
@@ -1,3 +1,6 @@
 myApp.controller('ekko', ['$scope', function($scope) {
-	$scope.abc = "my name is Ekko"
+	$scope.abc = "my name is Ekko";
+	$scope.win_per="46%";
+	$scope.usetime=180;
+	$scope.preficiency=12835;
 }])
