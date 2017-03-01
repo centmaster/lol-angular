8839b219c36963bb87e3fd56077cb84cf7355294
centmaster <ytchen@bupt.edu.cn>
2017-03-01 16:58:50 +0800

    add parse

--- js/controller/jinx.js
+++ js/controller/jinx.js
@@ -1,3 +1,14 @@
-myApp.controller('jinx123', ['$scope', function($scope) {
-	$scope.abc = "my name is Jinx"
+myApp.controller('jinx123', ['$scope', '$parse',function($scope,$parse) {
+	$scope.fizz = "Taunt Fizz falls asleep";
+	$scope.ezreal="Time for a true display of skill";
+	$scope.lux="Tactical decision summoner";
+	$scope.jinx="Rules are made to be broken... like buildings!";
+	$scope.ekko="People wasted a lot of time, and then wish for more."
+
+	$scope.$watch('hero',function(newVal,oldVal,scope){
+		if(newVal!==oldVal){
+			var parseFun=$parse(newVal);
+			scope.whattheysay = parseFun(scope);
+		}
+	});
 }])
